f1a39b7aa1e61f35585bb690b042f2e83af64c44
emjennett <emily.jennett@mail.utoronto.ca>
2023-11-22 13:59:24 -0500

    hehe

--- myscript.js
+++ myscript.js
@@ -1,2 +1,2 @@
-var html = '<a id= "name" href="https://github.com/emjennett/emjennett.github.io/blob/main/index.html"><h1>ej</h1></a><div class="circle"></div><a href="https://github.com/emjennett/emjennett.github.io/blob/main/about.html"> about </a><a href="https://github.com/emjennett/emjennett.github.io/blob/main/contact.html">contact</a><a href="https://github.com/emjennett/emjennett.github.io/blob/main/portfolio.html">portfolio</a></div>';
+var html = '<a id= "name" href="/index.html"><h1>ej</h1></a><div class="circle"></div><a href="/about.html"> about </a><a href="/contact.html">contact</a><a href="/portfolio.html">portfolio</a></div>';
 document.getElementById('sidebar').innerHTML = html;
