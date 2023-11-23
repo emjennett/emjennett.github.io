154fc90157c45e84076caa8f44d0b285790f2bb0
emjennett <emily.jennett@mail.utoronto.ca>
2023-11-23 01:35:02 -0500

    scroll animations

--- myscript.js
+++ myscript.js
@@ -1,2 +1,20 @@
 var html = '<a id= "name" href="/index.html"><h1>ej</h1></a><div class="circle"></div><a href="/about.html"> about </a><a href="/contact.html">contact</a><a href="/portfolio.html">portfolio</a></div>';
 document.getElementById('sidebar').innerHTML = html;
+
+function reveal() {
+    var reveals = document.querySelectorAll(".home");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
